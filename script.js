fb1a201eaa6f400f718397c0c046740f0db4eac3
Valeria48562 <vblagodatskaja@gmail.com>
2023-08-24 21:21:01 +0300

    commit

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
+const showFormButton = document.getElementById("showFormButton");
+const bookForm = document.getElementById("bookForm");
+const userBooks = document.getElementById("userBooks");
+
 const myLibrary = [];
-function Book(title, author, totalPages) {
-  this.title = title;
-  this.author = author;
-  this.totaPages = totalPages;
-  this.fullBook = function () {
-    return;
-  };
+
+function addBookToLibrary() {
+  const title = bookForm.elements.title.value;
+  const author = bookForm.elements.author.value;
+  const pages = bookForm.elements.pages.value;
+  myLibrary.push({ title, author, pages });
 }
-function addBookToLibrary() {}
+
+function resetValues() {
+  bookForm.elements.title.value = "";
+  bookForm.elements.author.value = "";
+  bookForm.elements.pages.value = "";
+}
+
+function displayBooks() {}
+
+showFormButton.addEventListener("click", function () {
+  bookForm.style.display = "block";
+});
+
+bookForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  addBookToLibrary();
+  resetValues();
+  bookForm.style.display = "none";
+  displayBooks();
+});
