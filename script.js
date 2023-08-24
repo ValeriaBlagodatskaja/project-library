a27387e68f576e351e2220913cae8e8e60c79142
Valeria48562 <vblagodatskaja@gmail.com>
2023-08-24 10:16:30 +0300

    commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const myLibrary = [];
+function Book(title, author, totalPages) {
+  this.title = title;
+  this.author = author;
+  this.totaPages = totalPages;
+  this.fullBook = function () {
+    return;
+  };
+}
+function addBookToLibrary() {}
